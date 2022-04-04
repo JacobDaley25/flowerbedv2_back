e80d9405cc796870699f634ac05aadad4b6d5036
JacobDaley25 <96602300+JacobDaley25@users.noreply.github.com>
2022-04-04 17:31:38 -0400

    added more links to whitelist

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -2,7 +2,10 @@ const whitelist = [
   'http://localhost:3001',
   'http://localhost:3000',
   'http://localhost:3000/register',
-  'http://localhost:3000/login'
+  'http://localhost:3000/login',
+  'http://localhost:3000/posts',
+  'http://localhost:3000/grows',
+  'http://localhost:3000/strains'
 ]
 const corsOptions = {
   origin : (origin, callback) => {
